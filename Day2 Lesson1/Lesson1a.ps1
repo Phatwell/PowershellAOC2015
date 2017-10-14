a2e7e9ac5e4337b761d95a8f5d576a4ee458734e
Phatwell <chrishatwell@gmail.com>
2017-10-14 19:24:12 +0100

    so far

---
+++ Day2 Lesson1/Lesson1a.ps1
@@ -0,0 +1,24 @@
+﻿#$input = get-content 'X:\chris\Documents\Powershell\Powershell2015\Day2 Lesson1\puzzleinput.txt'
+
+
+
+$input = '2x3x4'
+
+$dimensions = $input -split 'x'
+
+[int]$height = $dimensions[0]
+[int]$width = $dimensions[1]
+[int]$length = $dimensions[2]
+
+$measurement1 = ($height * $width)
+$measurement2 = ($width * $length)
+$measurement3 = ($height * $length)
+
+
+[int]$calc1 = 2 * $measurement1
+[int]$calc2 = 2 * $measurement2
+[int]$calc3 = 2 * $measurement3
+
+$extra = ($measurement1,$measurement2,$measurement3 | Measure-Object -Minimum).Minimum
+
+$surfacearea = $calc1 + $calc2 + $calc3 + $extra
