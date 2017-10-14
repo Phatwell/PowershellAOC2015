292ef26deb517a56c77ebe8f53d93a5c448c7173
Phatwell <chrishatwell@gmail.com>
2017-10-14 19:43:58 +0100

    almost there lesson a

--- Day2 Lesson1/Lesson1a.ps1
+++ Day2 Lesson1/Lesson1a.ps1
@@ -1,10 +1,7 @@
-﻿#$input = get-content 'X:\chris\Documents\Powershell\Powershell2015\Day2 Lesson1\puzzleinput.txt'
+﻿function get-CalculateSurfaceArea 
+{
 
-
-
-$input = '2x3x4'
-
-$dimensions = $input -split 'x'
+$dimensions = $instruction -split 'x'
 
 [int]$height = $dimensions[0]
 [int]$width = $dimensions[1]
@@ -14,7 +11,6 @@ $measurement1 = ($height * $width)
 $measurement2 = ($width * $length)
 $measurement3 = ($height * $length)
 
-
 [int]$calc1 = 2 * $measurement1
 [int]$calc2 = 2 * $measurement2
 [int]$calc3 = 2 * $measurement3
@@ -22,3 +18,13 @@ $measurement3 = ($height * $length)
 $extra = ($measurement1,$measurement2,$measurement3 | Measure-Object -Minimum).Minimum
 
 $surfacearea = $calc1 + $calc2 + $calc3 + $extra
+
+}
+
+$instrustions = get-content 'X:\chris\Documents\Powershell\Powershell2015\Day2 Lesson1\puzzleinput.txt'
+
+Foreach ($instruction in $instructions)
+    {
+    $results = $instruction | get-CalculateSurfaceArea 
+    }
+    
