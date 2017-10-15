76aa605853aa2085672f90ced0cc185857fdedb4
Phatwell <chrishatwell@gmail.com>
2017-10-15 17:57:35 +0100

    Lesson1b Complete

--- Day2 Lesson1/Lesson1b.ps1
+++ Day2 Lesson1/Lesson1b.ps1
@@ -1,32 +1,28 @@
-﻿#$instructions = get-content 'X:\chris\Documents\Powershell\Powershell2015\Day2 Lesson1\puzzleinput.txt'
+﻿$instructions = get-content 'X:\chris\Documents\Powershell\Powershell2015\Day2 Lesson1\puzzleinput.txt'
 
-#Foreach ($instruction in $instructions)
-  #  {
+$TotalRibbon = $null
 
-    $instruction = '2x3x4'
-
+Foreach ($instruction in $instructions)
+    {
+    
     $dimensions = $instruction -split 'x'
-
-    $smallperim = $dimensions | Sort-Object Number
 
     [int]$height = $dimensions[0]
     [int]$width = $dimensions[1]
     [int]$length = $dimensions[2]
 
+    $measurements = $height,$width,$length
 
+    $smallperim = $measurements | Sort-Object -Descending | Select -Last 2
 
-    #$measurement1 = ($height * $width)
-    #$measurement2 = ($width * $length)
-    #$measurement3 = ($height * $length)
+    [int]$ribbonwrap = [int]$smallperim[0] + [int]$smallperim[0] + [int]$smallperim[1] + [int]$smallperim[1]
 
-    #[int]$calc1 = 2 * $measurement1
-   # [int]$calc2 = 2 * $measurement2
-    #[int]$calc3 = 2 * $measurement3
+    $calc = $height * $width * $length
 
-    #$extra = ($measurement1,$measurement2,$measurement3 | Measure-Object -Minimum).Minimum
+    $Ribbon = $calc + $ribbonwrap
+    
+    $TotalRibbon += $Ribbon
 
-    #[int]$surfacearea = $calc1 + $calc2 + $calc3 + $extra
-#
-    #$total += $surfacearea
+    }
 
-   # }
+Write-Host "The total feet of Ribbon required is $TotalRibbon"
