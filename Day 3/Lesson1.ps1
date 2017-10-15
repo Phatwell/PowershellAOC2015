7bb13e2d4c25625fae3bf6ca50c6901ddb376d0b
Phatwell <chrishatwell@gmail.com>
2017-10-15 18:11:31 +0100

    Day 3

---
+++ Day 3/Lesson1.ps1
@@ -0,0 +1,11 @@
+﻿$instructions = get-content 'X:\chris\Documents\Powershell\Powershell2015\Day 3\puzzleinput.txt'
+
+[int]$count = 1
+
+foreach ($instruction in $instructions)
+    {
+    $count ++
+
+    $housecount = $count
+    }
+Write-Host "Houses receiving a present is $housecount"
