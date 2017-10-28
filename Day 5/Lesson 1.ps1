5b353fcbb1abd4dce62151aaaff4591522e8f983
Phatwell <chrishatwell@gmail.com>
2017-10-28 17:23:42 +0100

    Day 5 Regex nonsense

---
+++ Day 5/Lesson 1.ps1
@@ -0,0 +1,9 @@
+﻿$strings = get-content 'X:\chris\Documents\Powershell\Powershell2015\Day 5\puzzleinput.txt'
+
+$vowels = 'a','e','i','o','u'
+
+$strings -split "`n" | ?{ $_ -match "(.*[$vowels]){3}" -and $_ -match "(.)\1" -and $_ -notmatch "(ab|cd|pq|xy)" } | measure | % count
+
+ 
+
+        
---
+++ Day 5/Lesson 2.ps1
@@ -0,0 +1,3 @@
+﻿$strings = get-content 'X:\chris\Documents\Powershell\Powershell2015\Day 5\puzzleinput.txt'
+
+$strings -split "`n" | ?{ $_ -match "(..).*\1" -and $_ -match "(.).\1" } | measure | % count
