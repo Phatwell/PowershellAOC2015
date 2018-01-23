ee6799523cfbf2f4838f094ce8ae790252ab4bb6
Phatwell <chrishatwell@gmail.com>
2018-01-23 18:05:05 +0000

    powershellaoc2017

---
+++ PowershellAOC2017/Day 1/Day 1 Part 1.ps1
@@ -0,0 +1,35 @@
+﻿$instructions = Get-Content "D:\chris\PowershellAOC2017\Day 1\puzzleinput.txt"
+$results = $null
+[int]$i = -2
+$numbers = [int[]]($instructions -split '' | where {$_ -match '.'}) 
+
+
+foreach ($number in $numbers)
+    {
+    
+        $i++
+            if ($number -eq $numbers[$i])
+                {
+
+                $results += $number
+
+                }
+
+            else 
+                {
+                }
+    
+    }
+
+Write-host "The results are $results"
+
+
+
+
+#universal to lanuguages
+for ($i = 0; $i -lt $numbers.Length;$i++)
+{
+    if ($numbers[$i-1] -eq $numbers[$i])
+    {
+    }
+}
