4d9cad77523500550fa11ac641c458676c2b3e45
Lab User <labuser@ilab.local>
2018-01-30 17:59:57 +0000

    day2 barely complete

---
+++ PowershellAOC2017/Day 2/day 2 part1.ps1
@@ -0,0 +1,31 @@
+﻿$instructions = Get-Content "D:\chrishat\PowershellAOC2015\PowershellAOC2017\Day 2\puzzleinput.txt.txt"
+$instructionssplit = $instructions -split ([Environment]::NewLine)
+
+
+
+foreach($instruction in $instructions)
+[int[]]$splitinstructions = $instructions -split '\s+'
+
+
+    {
+        $sortedno = $splitinstruction | Sort-Object -Descending
+            
+
+
+
+    }
+
+
+
+
+
+
+
+    #foreach horizontal line.
+    #split each number (not individual)
+    #sort the number highest and lowest e.g 440, 13
+    #work out difference, add to variable 
+    #do same for all. 
+    #add at end.
+
+
